a804b63c1e00814e58243fac175e3766d91fdbe8
Connor Kam-Cheong <connorkc42@gmail.com>
2019-02-14 14:56:19 +0000

    Add golden master test

--- spec/gilded_rose.test.js
+++ spec/gilded_rose.test.js
@@ -1,7 +1,8 @@
-const {Shop, Item} = require('../src/gilded_rose.js')
+const Item = require('../src/item')
+const Shop = require('../src/shop')
 
 describe("Gilded Rose", () => {
-  test("creates an item called 'foo'", () => {
+  test("maintains the name of an item", () => {
     const gildedRose = new Shop([ new Item("foo", 0, 0) ])
     const items = gildedRose.updateQuality()
     expect(items[0].name).toBe("foo")
