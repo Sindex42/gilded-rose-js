bedbcf3d8d1a6156f879f086a0f7faefc9a71325
Connor Kam-Cheong <connorkc42@gmail.com>
2019-02-14 14:55:33 +0000

    Split gilded_rose into two files

--- src/shop.js
+++ src/shop.js
@@ -1,11 +1,3 @@
-class Item {
-  constructor (name, sellIn, quality) {
-    this.name = name
-    this.sellIn = sellIn
-    this.quality = quality
-  }
-}
-
 class Shop {
   constructor (items = []) {
     this.items = items
@@ -61,7 +53,4 @@ class Shop {
   }
 }
 
-module.exports = {
-  Item,
-  Shop
-}
+module.exports = Shop
