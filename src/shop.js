97730b1f6bd7b529c1748599993225409d43d7c5
Connor Kam-Cheong <connorkc42@gmail.com>
2019-02-15 15:20:52 +0000

    Refactor Aged Brie and Backstage passes

--- src/shop.js
+++ src/shop.js
@@ -6,46 +6,32 @@ class Shop {
   updateQuality () {
     this.items.forEach((item) => {
       if (item.name === 'Sulfuras, Hand of Ragnaros') { return }
+      item.sellIn = item.sellIn - 1
 
-      if (item.name !== 'Aged Brie' && item.name !== 'Backstage passes to a TAFKAL80ETC concert') {
+      if (item.name === 'Aged Brie') {
+        item.sellIn < 0 ? item.quality += 2 : item.quality += 1
+      }
+
+      else if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+        item.quality += 1
+        if (item.sellIn < 10) { item.quality += 1 }
+        if (item.sellIn < 5) { item.quality += 1 }
+        if (item.sellIn < 0) { item.quality = 0 }
+      }
+
+      else {
         if (item.quality > 0) {
           item.quality = item.quality - 1
         }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1
-          if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1
-              }
-            }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1
-              }
-            }
+        if (item.sellIn < 0) {
+          if (item.quality > 0) {
+            item.quality = item.quality - 1
           }
         }
       }
 
-      item.sellIn = item.sellIn - 1
+      if (item.quality > 50) { item.quality = 50 }
 
-      if (item.sellIn < 0) {
-        if (item.name !== 'Aged Brie') {
-          if (item.name !== 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.quality > 0) {
-              item.quality = item.quality - 1
-            }
-          } else {
-            item.quality = item.quality - item.quality
-          }
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1
-          }
-        }
-      }
     })
 
     return this.items
