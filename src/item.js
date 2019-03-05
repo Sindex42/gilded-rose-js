2ae92666d9ccf20ddbcb91eff84f4585aa7b4480
Connor Kam-Cheong <connorkc42@gmail.com>
2019-03-05 13:12:56 +0000

    Broken tests, start extraction

--- src/item.js
+++ src/item.js
@@ -1,8 +1,29 @@
+const legendaryItems = [
+  'Sulfuras, Hand of Ragnaros'
+]
+
+const specialItems = [
+  'Aged Brie',
+  'Backstage passes to a TAFKAL80ETC concert'
+]
+
 class Item {
   constructor (name, sellIn, quality) {
     this.name = name
     this.sellIn = sellIn
     this.quality = quality
+  }
+
+  updateSellIn () {
+    this.sellIn -= 1
+  }
+
+  isLegendary () {
+    if (legendaryItems.includes(this.name)) { return true }
+  }
+
+  isSpecial () {
+    if (this.name in specialItems) { return true }
   }
 }
 
--- src/shop.js
+++ src/shop.js
@@ -1,20 +1,16 @@
 const NonUnique = require('./non_unique')
-const { specialItems, legendaryItems } = require('./item_groups')
+const specialItems = require('./item_groups')
 
 class Shop {
   constructor (items = []) {
     this.items = items
   }
 
-  updateSellIn (item) {
-    item.sellIn -= 1
-  }
-
   advanceDay () {
     this.items.forEach((item) => {
-      if (this._isLegendary(item)) { return }
+      if (item.isLegendary()) { return }
 
-      this.updateSellIn(item)
+      item.updateSellIn()
       let newItem = this.assignItemType(item)
       newItem.updateQuality()
       item.quality = newItem.quality
@@ -25,21 +21,13 @@ class Shop {
 
   assignItemType (item) {
     let newItem
-    if (this._isSpecial(item)) {
+    if (item.isSpecial()) {
       newItem = new specialItems[item.name](item.name, item.sellIn, item.quality)
     } else {
       newItem = new NonUnique(item.name, item.sellIn, item.quality)
     }
 
     return newItem
-  }
-
-  _isLegendary (item) {
-    if (legendaryItems.includes(item.name)) { return true }
-  }
-
-  _isSpecial (item) {
-    if (item.name in specialItems) { return true }
   }
 }
 
